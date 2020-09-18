a4550a37934512d467745b3a4ea4c11f17538f67
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-09-18 13:05:09 -0600

    proto blog cards

--- src/components/post.vue
+++ src/components/post.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="col-4 p-1">
-      <div class="card">
-        <p>test title</p>
-        <p class="text-center">{{blogData.id}}</p>
+      <div class="row d-flex">
+        <div class="col-3 card">
+          <h6>{{blogData.creator.name}}</h6>
+          <img :src="blogData.creator.picture" alt="">
+        </div>
+        <div class="card col-8">
+        <p>{{blogData.title}}</p>
+        <img :src="blogData.imgUrl" alt="" class="card-img">
+        <p class="text-center">{{blogData.body}}</p>
+        </div>
+      
       </div>
     </div>
 </template>
@@ -18,6 +26,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+img{
+  border-radius: 50%;
+}
 
 </style>
